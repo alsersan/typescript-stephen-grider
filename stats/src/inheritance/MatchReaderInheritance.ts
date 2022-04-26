92dc440bb0aef26a48df93eba26197426fbec07c
Álvaro Serrano <alsersan20@gmail.com>
2022-04-27 00:02:41 +0200

    Move MatchData type to a separate file

--- stats/src/inheritance/MatchReaderInheritance.ts
+++ stats/src/inheritance/MatchReaderInheritance.ts
@@ -1,8 +1,7 @@
 import { CsvFileReader } from './CsvFileReaderInheritance';
 import { MatchResult } from '../MatchResult';
 import { dateStringToDate } from '../utils';
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+import { MatchData } from '../MatchData';
 
 export class MatchReader extends CsvFileReader<MatchData> {
   constructor(public filename: string) {
