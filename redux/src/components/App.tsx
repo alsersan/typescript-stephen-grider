f5c2b0e6129e396c89844db91af1bb9a038b64b4
Álvaro Serrano <alsersan20@gmail.com>
2022-05-04 20:56:23 +0200

    Create RepositoriesList component template

--- redux/src/components/App.tsx
+++ redux/src/components/App.tsx
@@ -1,5 +1,16 @@
+import { Provider } from 'react-redux';
+import { store } from '../state';
+import RepositoriesList from './RepositoriesList';
+
 function App() {
-  return <div className="App">Hello React</div>;
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Search for a package</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
---
+++ redux/src/components/RepositoriesList.tsx
@@ -0,0 +1,10 @@
+function RepositoriesList() {
+  return (
+    <form>
+      <input type="text" />
+      <button>Search</button>
+    </form>
+  );
+}
+
+export default RepositoriesList;
