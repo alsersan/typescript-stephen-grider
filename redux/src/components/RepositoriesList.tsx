1c20c3553265cb966e1fafd8cccffa6394c91383
Álvaro Serrano <alsersan20@gmail.com>
2022-05-05 07:54:32 +0200

    Add submit logic to RepositoriesList

--- redux/src/components/RepositoriesList.tsx
+++ redux/src/components/RepositoriesList.tsx
@@ -1,10 +1,22 @@
-function RepositoriesList() {
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { actionCreators } from '../state';
+
+const RepositoriesList = () => {
+  const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch<any>(actionCreators.searchRepositories(term));
+  };
+
   return (
-    <form>
-      <input type="text" />
+    <form onSubmit={handleSubmit}>
+      <input value={term} onChange={(e) => setTerm(e.target.value)} />
       <button>Search</button>
     </form>
   );
-}
+};
 
 export default RepositoriesList;
