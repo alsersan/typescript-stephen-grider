6a7cb72d41d2cf33aafa5e8a0901c3c5110847b2
Álvaro Serrano <alsersan20@gmail.com>
2022-05-06 19:57:05 +0200

    Create store and add repositoriesReducer

--- redux/src/components/App.tsx
+++ redux/src/components/App.tsx
@@ -1,10 +1,11 @@
 import { Provider } from 'react-redux';
 import { store } from '../state';
+import { storeToolkit } from '../state-toolkit/store';
 import RepositoriesList from './RepositoriesList';
 
 function App() {
   return (
-    <Provider store={store}>
+    <Provider store={storeToolkit}>
       <div>
         <h1>Search for a package</h1>
         <RepositoriesList />
